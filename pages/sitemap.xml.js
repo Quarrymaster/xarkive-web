cf03d0a5c827aac7c9ae3072f30de6d9efc9beee
Jarrad Brevi <jarradbrevi@Jarrads-MacBook-Pro.local>
2026-04-13 22:06:26 +1000

    Add sitemap, robots.txt and document

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,29 @@
+const SITE_URL = 'https://xarkive.com'
+
+function generateSiteMap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${SITE_URL}</loc>
+      <changefreq>hourly</changefreq>
+      <priority>1.0</priority>
+    </url>
+    <url>
+      <loc>${SITE_URL}/methodology</loc>
+      <changefreq>monthly</changefreq>
+      <priority>0.8</priority>
+    </url>
+  </urlset>`
+}
+
+function SiteMap() {}
+
+export async function getServerSideProps({ res }) {
+  const sitemap = generateSiteMap()
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(sitemap)
+  res.end()
+  return { props: {} }
+}
+
+export default SiteMap
